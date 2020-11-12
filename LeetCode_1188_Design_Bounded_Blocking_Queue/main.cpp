5235f837c469b0671afebeec30defe37b058e89d
Dongfang334425 <dongfang334425@163.com>
2020-11-12 12:19:08 +0800

    first commit

---
+++ LeetCode_1188_Design_Bounded_Blocking_Queue/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace::std;
+
+class BoundedBlockingQueue {
+public:
+	BoundedBlockingQueue(int capacity)
+	{
+
+	}
+
+	void enqueue(int element)
+	{
+
+	}
+
+	int dequeue()
+	{
+
+	}
+
+	int size()
+	{
+
+	}
+};
+int main()
+{
+	return system("pause");
+}
